3f7b77eee41a97a965b67884c6ac4824ee71393b
Qiaolun Zhang <qiaolunzhang@gmail.com>
2018-05-03 14:11:12 +0800

    add class

---
+++ icn_class/client_class.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+import socket, select, string, sys
+HOST = '127.0.0.1'
+PORT = 5250
+ID = '1'
+
+class ChatClient:
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.settimeout(2)
+        self.connect()
+
+    def connect(self):
+        try:
+            self.client_socket.connect((HOST, PORT))
+            self.client_socket.send(ID)
+        except Exception,e:
+            print 'Unable to connect because of %s'%e
+            sys.exit()
+        else:
+            print 'Connected to remote host. Start sending messages'
+            self.prompt()
+
+
+    def prompt(self):
+        sys.stdout.write('\n<You> ')
+        sys.stdout.flush()
+
+    def socket_handler(self):
+        while 1:
+            rlist = [sys.stdin, self.client_socket]  # 接收列表
+            read_list, write_list, error_list = select.select(rlist, [], [], 2)
+            for sock in read_list:
+                # incoming message from remote server
+                if sock == self.client_socket:
+                    data = sock.recv(4096)
+                    if not data:
+                        print '\nDisconnected from chat server'
+                        sys.exit()
+                    else:
+                        # print data
+                        sys.stdout.write(data)
+                        self.prompt()
+
+                # user entered a message
+                else:
+                    msg = sys.stdin.readline()
+                    remote_id = raw_input("Please input remote id:")
+                    msg_send = "%s||%s"%(remote_id,msg)
+                    self.client_socket.send(msg_send)
+                    self.prompt()
+
+if __name__ == '__main__':
+    chat_client_obj = ChatClient()
+    chat_client_obj.socket_handler()
--- icn_class/server_class.py
+++ icn_class/server_class.py
@@ -1,175 +1,87 @@
-import math
-import struct
-import socket
+# -*- coding: utf-8 -*-
 import select
-import threading
+import socket
 
-_HOST = '127.0.0.1'  # defines the host as "localhost"
-_PORT = 10000        # defines the port as "10000"
+inBufSize = 4096
+outBufSize = 4096
+CONNECTION_LIST = []
 
-class ChatServer(threading.Thread):
-    """
-    Defines the chat server as a Thread.
-    """
+class ChatServer:
+    def __init__(self,port=5250):
+        # todo 使用socketserver来写
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverSocket.bind(('', port))
+        self.serverSocket.listen(5)
+        print "server wait for connect...."
+        self.socketsMap = {}  # socket session字典 id : socket
+        self.idMap = {} #socket session 字典 socket:id
+        CONNECTION_LIST.append(self.serverSocket)
 
-    MAX_WAITING_CONNECTIONS = 10
-    RECV_BUFFER = 4096
-    RECV_MSG_LEN = 4
+    def login(self,id,sock):#新用户登录
+        print "%s login"%id
+        self.socketsMap[id] = sock
+        self.idMap[sock] = id
+        sock.send('hello %s,you login successed'%id)
+        CONNECTION_LIST.append(sock)#要在这里把socket加进来才行
 
-    def __init__(self, host, port):
-        """
-        Initializes a new ChatServer.
+    def chat(self,sock):#点对点聊天，发送消息格式id||信息
+        try:
+            data = sock.recv(inBufSize)
+        except Exception:
+            sock.send("remote is offline")
+            sock.close()
+        else:
+            remote_id = data.split('||')[0]
+            message = data.split('||')[1]
+            print "id = %s,message = %s"%(remote_id,message)
+            local_id = self.idMap[sock]
+            if remote_id == 'all':
+                self.broadcast(local_id,message)
+            else:
+                self.p2psend(local_id,message,remote_id)
 
-        :param host: the host on which the server is bounded
-        :param port: the port on which the server is bounded
-        """
-        threading.Thread.__init__(self)
-        self.host = host
-        self.port = port
-        self.connections = []  # collects all the incoming connections
-        self.running = True  # tells whether the server should run
+    def p2psend(self,local_id,message,remote_id):
+        remote_socket = self.socketsMap[remote_id]
+        message_send = "%s said : %s" % (local_id, message)
+        try:
+            remote_socket.sendall(message_send)
+        except Exception,e:
+            print e
+            remote_socket.close()
+            CONNECTION_LIST.remove(remote_socket)
 
-    def _bind_socket(self):
-        """
-        Creates the server socket and binds it to the given host and port.
-        """
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen(self.MAX_WAITING_CONNECTIONS)
-        self.connections.append(self.server_socket)
-
-    def _send(self, sock, msg):
-        """
-        Prefixes each message with a 4-byte length before sending.
-
-        :param sock: the incoming socket
-        :param msg: the message to send
-        """
-        # Packs the message with 4 leading bytes representing the message length
-        msg = struct.pack('>I', len(msg)) + msg
-        # Sends the packed message
-        sock.send(msg)
-
-    def _receive(self, sock):
-        """
-        Receives an incoming message from the client and unpacks it.
-
-        :param sock: the incoming socket
-        :return: the unpacked message
-        """
-        data = None
-        # Retrieves the first 4 bytes from the message
-        tot_len = 0
-        while tot_len < self.RECV_MSG_LEN:
-            msg_len = sock.recv(self.RECV_MSG_LEN)
-            tot_len += len(msg_len)
-        # If the message has the 4 bytes representing the length...
-        if msg_len:
-            data = ''
-            # Unpacks the message and gets the message length
-            msg_len = struct.unpack('>I', msg_len)[0]
-            tot_data_len = 0
-            while tot_data_len < msg_len:
-                # Retrieves the chunk i-th chunk of RECV_BUFFER size
-                chunk = sock.recv(self.RECV_BUFFER)
-                # If there isn't the expected chunk...
-                if not chunk:
-                    data = None
-                    break # ... Simply breaks the loop
-                else:
-                    # Merges the chunks content
-                    data += chunk
-                    tot_data_len += len(chunk)
-        return data
-
-
-    def _broadcast(self, client_socket, client_message):
-        """
-        Broadcasts a message to all the clients different from both the server itself and
-        the client sending the message.
-
-        :param client_socket: the socket of the client sending the message
-        :param client_message: the message to broadcast
-        """
-        for sock in self.connections:
-            is_not_the_server = sock != self.server_socket
-            is_not_the_client_sending = sock != client_socket
-            if is_not_the_server and is_not_the_client_sending:
-                try :
-                    self._send(sock, client_message)
-                except socket.error:
-                    # Handles a possible disconnection of the client "sock" by...
-                    sock.close()  # closing the socket connection
-                    self.connections.remove(sock)  # removing the socket from the active connections list
-
-    def _run(self):
-        """
-        Actually runs the server.
-        """
-        while self.running:
-            # Gets the list of sockets which are ready to be read through select non-blocking calls
-            # The select has a timeout of 60 seconds
-            try:
-                ready_to_read, ready_to_write, in_error = select.select(self.connections, [], [], 60)
-            except socket.error:
+    def broadcast(self,local_id,message):
+        for sock in CONNECTION_LIST:
+            if sock == self.serverSocket:
                 continue
             else:
-                for sock in ready_to_read:
-                    # If the socket instance is the server socket...
-                    if sock == self.server_socket:
-                        try:
-                            # Handles a new client connection
-                            client_socket, client_address = self.server_socket.accept()
-                        except socket.error:
-                            break
-                        else:
-                            self.connections.append(client_socket)
-                            print "Client (%s, %s) connected" % client_address
+                try:
+                    message_send = "%s said : %s" % (local_id, message)
+                    sock.send(message_send)
+                except Exception,e:
+                    print e
+                    sock.close()
+                    CONNECTION_LIST.remove(sock)
+                    continue
 
-                            # Notifies all the connected clients a new one has entered
-                            self._broadcast(client_socket, "\n[%s:%s] entered the chat room\n" % client_address)
-                    # ...else is an incoming client socket connection
-                    else:
-                        try:
-                            data = self._receive(sock) # Gets the client message...
-                            if data:
-                                # ... and broadcasts it to all the connected clients
-                                self._broadcast(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)
-                        except socket.error:
-                            # Broadcasts all the connected clients that a clients has left
-                            self._broadcast(sock, "\nClient (%s, %s) is offline\n" % client_address)
-                            print "Client (%s, %s) is offline" % client_address
-                            sock.close()
-                            self.connections.remove(sock)
-                            continue
-        # Clears the socket connection
-        self.stop()
+    def socet_handle(self):
+        while 1:
+            # Get the list sockets which are ready to be read through select
+            read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST, [], [])
+            for sock in read_sockets:
+                # New connection
+                if sock == self.serverSocket:#用户通过主socket（即服务器开始创建的 socket，一直处于监听状态）来登录
+                    # Handle the case in which there is a new connection recieved through server_socket
+                    sockfd, addr = self.serverSocket.accept()
+                    id = sockfd.recv(100)
+                    self.login(id,sockfd)
+                else:
+                    self.chat(sock)
 
-    def run(self):
-        """Given a host and a port, binds the socket and runs the server.
-        """
-        self._bind_socket()
-        self._run()
-
-    def stop(self):
-        """
-        Stops the server by setting the "running" flag before closing
-        the socket connection.
-        """
-        self.running = False
-        self.server_socket.close()
-
-
-def main():
-    """
-    The main function of the program. It creates and runs a new ChatServer.
-    """
-    chat_server = ChatServer(_HOST, _PORT)
-    chat_server.start()
-
+    def main(self):
+        self.socet_handle()
+        self.serverSocket.close()
 
 if __name__ == '__main__':
-    """The entry point of the program. It simply calls the main function.
-    """
-    main()
+    chat_server_obj = ChatServer()
+    chat_server_obj.main()
