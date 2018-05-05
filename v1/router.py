08efacfbc3286f64691d193b323c3dd85189af9c
Qiaolun Zhang <qiaolunzhang@gmail.com>
2018-05-05 19:30:22 +0800

    test send back

--- v1/router.py
+++ v1/router.py
@@ -1,38 +1,49 @@
-import math
-import struct
-import socket
+# -*- coding: utf-8 -*-
 import select
-import threading
+import socket
+import struct
 
-#@todo 读取文件参数
-_HOST = '127.0.0.1'  # defines the host as "localhost"
-_PORT = 10000        # defines the port as "10000"
+_HOST = '127.0.0.1'
+_PORT = 10000
 
-class Router(threading.Thread):
-    """
-    Defines the router as a Thread.
-    """
 
+class Router:
     MAX_WAITING_CONNECTIONS = 100
     RECV_BUFFER = 4096
     RECV_MSG_LEN = 4
+    RECV_MSG_TYPE_LEN = 4
 
     def __init__(self, host, port):
-        """
-        Initializes a new Router.
+        # store the fib form
+        self.fib_dic = {}
+        # store the pit form
+        self.pit_dic = {}
+        # store the cs form
+        self.cs_dic = {}
 
-        :param host: the host on which the server is bounded
-        :param port: the port on which the server is bounded
-        """
-        threading.Thread.__init__(self)
         self.host = host
         self.port = port
-        self.connections = []  # collects all the incoming connections
-        self.running = True  # tells whether the server should run
+        self.connections = [] # collects all the incoming connections
+        self.load_config()
+        self._run()
+
+    def load_config(self):
+        try:
+            with open('./config/router.conf') as f:
+                for line in f:
+                    if line[0] != '#':
+                        line = line.split()
+                        self.fib_dic[line[0]] = line[1]
+
+            #print(self.fib_dic)
+        except:
+            print("Failed to load the config file")
+            raise SystemExit
 
     def _bind_socket(self):
         """
-        Creates the server socket and binds it to the given host and port.
+        Create the sever socket and bind it to the given host and port
+        :return:
         """
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -40,39 +51,44 @@ class Router(threading.Thread):
         self.server_socket.listen(self.MAX_WAITING_CONNECTIONS)
         self.connections.append(self.server_socket)
 
-    def _send(self, sock, msg):
-        """
-        Prefixes each message with a 4-byte length before sending.
-
-        :param sock: the incoming socket
-        :param msg: the message to send
-        """
-        # Packs the message with 4 leading bytes representing the message length
-        msg = struct.pack('>I', len(msg)) + msg
-        # Sends the packed message
-        sock.send(msg)
-
     def _receive(self, sock):
         """
-        Receives an incoming message from the client and unpacks it.
-
-        :param sock: the incoming socket
-        :return: the unpacked message
+        first get length
+        then get type
+        process
+        :return:
         """
+        #@todo send the data to the next route
         data = None
-        # Retrieves the first 4 bytes from the message
+        # Retrieves the first 4 bytes form message
         tot_len = 0
+        msg_len = 0
+        typ_len = 0
+
+        # 得到数据包的总长度
         while tot_len < self.RECV_MSG_LEN:
             msg_len = sock.recv(self.RECV_MSG_LEN)
             tot_len += len(msg_len)
-        # If the message has the 4 bytes representing the length...
+        tot_len = 0
+        print("ok to get msg_len")
+        # 得到数据包的类型
+        while tot_len < self.RECV_MSG_TYPE_LEN:
+            typ_len = sock.recv(self.RECV_MSG_TYPE_LEN)
+            tot_len += len(typ_len)
+        print("ok to get typ_len")
+        if typ_len:
+            try:
+                packet_type = struct.unpack('>I', typ_len)[0]
+                print("The package type is ", packet_type)
+            except:
+                print("Failed to unpack the package type")
         if msg_len:
             data = ''
             try:
                 # Unpacks the message and gets the message length
-                msg_len = struct.unpack('>I', msg_len)[0]
+                msg_len_unpack = struct.unpack('>I', msg_len)[0]
                 tot_data_len = 0
-                while tot_data_len < msg_len:
+                while tot_data_len < msg_len_unpack:
                     # Retrieves the chunk i-th chunk of RECV_BUFFER size
                     chunk = sock.recv(self.RECV_BUFFER)
                     # If there isn't the expected chunk...
@@ -83,36 +99,20 @@ class Router(threading.Thread):
                         # Merges the chunks content
                         data += chunk
                         tot_data_len += len(chunk)
+                # 原始的整个数据包
+                data_origin = msg_len + typ_len
+                sock.send(data)
+                print("The received data is ", data, 'the length is', len(data))
             except:
-                print("Failed to unpack the packet")
-
-        return data
-
-
-    def _broadcast(self, client_socket, client_message):
-        """
-        Broadcasts a message to all the clients different from both the server itself and
-        the client sending the message.
-
-        :param client_socket: the socket of the client sending the message
-        :param client_message: the message to broadcast
-        """
-        for sock in self.connections:
-            is_not_the_server = sock != self.server_socket
-            is_not_the_client_sending = sock != client_socket
-            if is_not_the_server and is_not_the_client_sending:
-                try :
-                    self._send(sock, client_message)
-                except socket.error:
-                    # Handles a possible disconnection of the client "sock" by...
-                    sock.close()  # closing the socket connection
-                    self.connections.remove(sock)  # removing the socket from the active connections list
+                print("Failed to unpack the packet length")
 
     def _run(self):
-        """
-        Actually runs the server.
-        """
-        while self.running:
+        #todo 对于新来的socket，开一个线程，进行计时，如果超时没有收到另一个方向发回来的包，就关闭这个线程
+        self._bind_socket()
+        while True:
+            """
+            Actually runs the server.
+            """
             # Gets the list of sockets which are ready to be read through select non-blocking calls
             # The select has a timeout of 60 seconds
             try:
@@ -121,62 +121,33 @@ class Router(threading.Thread):
                 continue
             else:
                 for sock in ready_to_read:
-                    # If the socket instance is the server socket...
                     if sock == self.server_socket:
-                        try:
-                            # Handles a new client connection
-                            client_socket, client_address = self.server_socket.accept()
-                        except socket.error:
-                            break
-                        else:
-                            self.connections.append(client_socket)
-                            print "Client (%s, %s) connected" % client_address
-
-                            # Notifies all the connected clients a new one has entered
-                            self._broadcast(client_socket, "\n[%s:%s] entered the chat room\n" % client_address)
-                    # ...else is an incoming client socket connection
+                        if sock == self.server_socket:
+                            try:
+                                # Handles a new client connection
+                                client_socket, client_address = self.server_socket.accept()
+                            except socket.error:
+                                break
+                            else:
+                                self.connections.append(client_socket)
+                                print "Client (%s, %s) connected" % client_address
+                        # ... else is an incoming client socket connection
                     else:
                         try:
-                            data = self._receive(sock) # Gets the client message...
-                            if data:
-                                print "The received data is " + data
-                                # ... and broadcasts it to all the connected clients
-                                # self._broadcast(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)
-                                #@todo 在这里写一个发送的给下一条的函数
+                            #next_route_ip, data = self._receive(sock)
+                            self._receive(sock)
                         except socket.error:
-                            # Broadcasts all the connected clients that a clients has left
-                            self._broadcast(sock, "\nClient (%s, %s) is offline\n" % client_address)
-                            print "Client (%s, %s) is offline" % client_address
+                            #print("Client is offline" % client_address)
                             sock.close()
                             self.connections.remove(sock)
                             continue
-        # Clears the socket connection
-        self.stop()
-
-    def run(self):
-        """Given a host and a port, binds the socket and runs the server.
-        """
-        self._bind_socket()
-        self._run()
-
-    def stop(self):
-        """
-        Stops the server by setting the "running" flag before closing
-        the socket connection.
-        """
-        self.running = False
-        self.server_socket.close()
 
 
-def main():
-    """
-    The main function of the program. It creates and runs a new ChatServer.
-    """
-    router = Router(_HOST, _PORT)
-    router.start()
+
+        #@todo 下面这个可以先放着
+        # 来一个新的包，首先发送，然后开一个线程，如果时间到了还没有结束
+        # 首先删除pit中的记录，然后再关闭
 
 
-if __name__ == '__main__':
-    """The entry point of the program. It simply calls the main function.
-    """
-    main()
+
+r = Router(_HOST, _PORT)
